4b62bb01f8190d6946ef49fbc0d11a484523e149
Eden_one <30387806+sun111sunshine@users.noreply.github.com>
2018-02-04 16:29:47 +0800

    Delete common.js

--- libs/common.js
+++
@@ -1,9 +0,0 @@
-const crypto=require('crypto');
-module.exports={
-    MD5_SUFFIX: 'FDSW$t34tregt5tO&$德玛西亚',
-    md5: function (str){
-        var obj=crypto.createHash('md5');
-        obj.update(str);
-        return obj.digest('hex');
-    }
-};
