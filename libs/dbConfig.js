59fc192aa738151088266bf30315aa608968b6b3
Eden_one <30387806+sun111sunshine@users.noreply.github.com>
2018-02-04 16:30:45 +0800

    Delete dbConfig.js

--- libs/dbConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-    {
-        mysql:{
-            user: 'root',
-            password: '520956wjx',
-            database: 'blog',
-            host:'localhost'
-        }
-    };
